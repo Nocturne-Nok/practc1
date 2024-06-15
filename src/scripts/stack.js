5b4fdcd853150ad10781a1daca7fdea757b61839
Nocturne-Nok <mol4r4ag1@gmail.com>
2024-06-15 17:18:37 +0300

    UwU

--- src/scripts/stack.js
+++ src/scripts/stack.js
@@ -1,54 +1,154 @@
+let personOne;
+let personTwo;
+let n;
+let phiN;
+let e;
+let d;
+let tmpD;
+let privateKey;
+let publicKey;
+let alphabet = ['а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м',
+    'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я', ' ', '.', ',', '?', '!', '-'];
 
+function algorithmRSA() {
+    n = 0;
+    phiN = 0;
+    e = 0;
+    d = 0;
 
-console.log("hello");
+    if (inputPersonOne.value === "" || inputPersonTwo.value === "") {
+        alert("Введіть значення");
+    } else {
+        console.log("===========================");
 
+        //
+        personOne = parseInt(inputPersonOne.value);
+        while (!simplicityCheck(personOne)) {
+            personOne ++;
+        }
+        console.log("personOne = " + personOne);
 
-//document.writeln("ssssssssssssssssss");
-let stack1 = "";
-let N = 12;
+        //
+        personTwo = parseInt(inputPersonTwo.value);
+        while (!simplicityCheck(personTwo)) {
+            personTwo ++;
+        }
+        console.log("personTwo = " + personTwo);
 
-function PushOur() {
+        n = personOne * personTwo;
+        console.log("n =" + n);
 
-    if (stack1.split(", ").length  <= N) {
-        a = document.getElementById("pushValue").value;
-        stack1 = stack1 + ", " + a;
-        console.log(stack1);
-        document.getElementById("displayStack").textContent = stack1;
-    }
-    else
-    {
-        alert("stack full");
-    }
-}
+        phiN = (personOne - 1) * (personTwo - 1);
+        console.log("phiN = " + phiN);
 
-function PopOur() {
+        //
+        for (let i = Math.round(phiN); i > 2; i--) {
+            if (simplicityCheck(i) === true) {
+                e = i;
 
-    if ( ! (stack1.split(', ').length  === 1)) {
-        let tmpStack = "";
-        alert("Pop element " + stack1.split(', ') [stack1.split(', ').length - 1]);
-
-        for (let i = 1; i < stack1.split(', ').length - 1; i++) {
-            tmpStack += ', ' + stack1.split(', ')[i];
+                //
+                for (let j = 2; j < phiN; j++) {
+                    tmpD = (phiN * j + 1) / e;
+                    if (Number.isInteger(tmpD) === true) {
+                        if (tmpD !== e && simplicityCheck(tmpD) === true && (e * tmpD) % phiN === 1) {
+                            d = tmpD;
+                            break;
+                        }
+                    }
+                }
+                if (d !== 0 && d !== e) {
+                    break;
+                }
+            }
+        }
+        if (d === 0) {
+            return alert("Помилка! Неможливо знайти ключ");
         }
 
+        console.log("e = " + e);
+        console.log("d = " + d);
+        console.log("e*d = " + e * d);
+        console.log("e*d % phiN = " + (e * d) % phiN);
 
-        stack1 = tmpStack;
-        console.log(stack1);
-        document.getElementById("displayStack").textContent = stack1;
-    }
-    else
-    {
-        alert("Stack empty");
+        privateKey = "PrivateKey: ( " + d + ", " + n + ")";
+        publicKey = "PublicKey: ( " + e + ", " + n + ")";
+        document.getElementById("privateKeyLabel").textContent = privateKey;
+        document.getElementById("publicKeyLabel").textContent = publicKey;
     }
 }
 
-
-function SizeOur() {
-    alert("Size stack " + (stack1.split(', ').length - 1));
+function simplicityCheck(number) {
+    //
+    if (number <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-function ClearOur() {
-    stack1 = "";
-    document.getElementById("displayStack").textContent = stack1;
-    alert("Cleared ");
+function encryption() {
+    //
+    if (inputPersonOne.value === "" || inputPersonTwo.value === "") {
+        alert("Згенеруйте ключі");
+    } else {
+        if (inputText.value === "") {
+            alert("Введіть текст");
+        }else {
+            console.log("==encryption==");
+            let clearText = (inputText.value).toLowerCase().split('');
+            console.log(clearText)
+            let cipherText = [];
+            for (let i = 0; i < clearText.length; i++) {
+                let letter = clearText[i];
+                let letterIndex
+                for (let j = 0; j < alphabet.length; j++) {
+                    if (letter === alphabet[j]) {
+                        letterIndex = j;
+                        break;
+                    }
+                }
+
+                if (letterIndex == null) {
+                    return alert("Введіть українською");
+                }
+                let exponentiation = (BigInt(letterIndex) ** BigInt(e));
+                console.log(letterIndex + "^e = " + exponentiation);
+                let cipherLetter = exponentiation % BigInt(n);
+                console.log(letter + " = " + letterIndex + " = " + cipherLetter);
+                cipherText.push(cipherLetter);
+            }
+            console.log(cipherText);
+            document.getElementById("cipherTextLabel").textContent = cipherText;
+        }
+    }
+}
+
+function decryption() {
+    if (cipherTextLabel.value === "") {
+        alert("Зашифруйте текст");
+    } else {
+        console.log("==decryption==");
+        let decryptionText = [];
+        let cipherText = (cipherTextLabel.textContent).split(",");
+        for (let i = 0; i < cipherText.length; i++) {
+            let decryptionLetter;
+            let exponentiation = (BigInt(letterIndex) ** BigInt(d));
+            console.log(cipherText + "^d = " + exponentiation);
+            let decryptionLetterIndex = exponentiation % BigInt(n);
+            console.log(decryptionLetterIndex);
+            for (let j = 0; j < alphabet.length; j++) {
+                if (decryptionLetterIndex === BigInt(j)) {
+                    decryptionLetter = alphabet[j];
+                    break;
+                }
+            }
+            decryptionText.push(decryptionLetter);
+        }
+        console.log(decryptionText);
+        document.getElementById("decryptionTextLabel").textContent = decryptionText;
+    }
 }
